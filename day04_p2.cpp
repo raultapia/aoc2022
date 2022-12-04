91194e2c2556a98e8d0769c841da594f8efafe86
Raul Tapia <raultapia@us.es>
2022-12-04 12:00:20 +0100

    Day 04

---
+++ day04_p2.cpp
@@ -0,0 +1,17 @@
+#include <fstream>
+#include <iostream>
+
+int main(int argc, char const *argv[]) {
+  std::ifstream file("input04.txt");
+  unsigned long overlap = 0;
+  std::string line;
+  std::pair<int,int> a,b;
+
+  while(file >> line){
+    sscanf(line.c_str(), "%d-%d,%d-%d", &a.first, &a.second, &b.first, &b.second);
+    if(!(a.second < b.first || b.second < a.first)) overlap++;
+  }
+
+  std::cout << "Total = " << overlap << std::endl;
+  return 0;
+}
