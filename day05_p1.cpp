f6f29a779e22068507de832b01741be4529735b0
Raul Tapia <raultapia@us.es>
2022-12-05 18:51:05 +0100

    Day 05

---
+++ day05_p1.cpp
@@ -0,0 +1,37 @@
+#include <fstream>
+#include <iostream>
+#include <stack>
+#include <vector>
+
+int main(int argc, char const *argv[]) {
+    std::ifstream file("input05.txt");
+    std::stack<std::string> lines;
+    std::string line;
+    unsigned int cnt, from, to;
+
+    while(std::getline(file, line)) {
+        if(line.empty()) break;
+        lines.push(line);
+    }
+
+    const size_t n = (lines.top().end()[-2])-'0'; lines.pop();
+    std::vector<std::stack<char> > stack(n);
+    while(!lines.empty()) {
+        for(size_t i = 0; i < n; i++) {
+            char c = lines.top()[4*i+1];
+            if(c > 64) stack[i].push(c);
+        }
+        lines.pop();
+    }
+
+    while(std::getline(file, line)) {
+        sscanf(line.c_str(), "move %d from %d to %d", &cnt, &from, &to);
+        for(unsigned int i = 0; i < cnt; i++) {
+            stack[to-1].push(stack[from-1].top());
+            stack[from-1].pop();
+        }
+    }
+
+    std::cout << "Solution = "; for(std::stack<char> s : stack) std::cout << s.top(); std::cout << std::endl;
+    return 0;
+}
